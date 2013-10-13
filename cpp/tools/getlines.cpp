09fb747cb1b0c1508f4a7000183d78aff72fffae
wyy <wuyanyi09@gmail.com>
2013-10-13 10:48:46 +0800

    modify .bashrc

--- cpp/tools/getlines.cpp
+++ cpp/tools/getlines.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
+#include <cstdlib>
 using namespace std;
 
 int main(int argc, char** argv)
 {
     if(argc < 2)
     {
-        cout<<"usage: "<<argv[0]<<" <filename>"<<endl;
+        cout<<"usage: "<<argv[0]<<" <filename> <line_offset> <line_cnt>"<<endl;
     }
     ifstream ifs(argv[1]);
     string line;
-    int lineno =0;
-    while(getline(ifs, line, '\0'))
+    int lineno =atoi(argv[2]);
+	int linecnt = atoi(argv[3]);
+	string tmp;
+	for(int i =0;i < lineno;i++)
+	{
+		getline(ifs,tmp,'\0');
+	}
+    while(linecnt > 0 && getline(ifs, line, '\0'))
     {
         for(int i = 0; i< line.size(); i++)
         {
-            if(line[i]=='\n')
-              line[i] = '/';
+            if(line[i]=='\n' || line[i]=='\t' || line[i] == '\r')
+              line[i] = ' ';
         }
         cout<<line<<endl;
+		linecnt--;
     }
     
     return 0;

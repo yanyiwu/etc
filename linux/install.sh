86b2023b612d0c3d67df4eaf5033a423411f8355
wyy <wuyanyi09@gmail.com>
2013-10-18 08:31:19 -0700

    add zsh

--- linux/install.sh
+++ linux/install.sh
@@ -2,4 +2,5 @@
 mkdir ~/.trash
 cp -r .gitconfig .inputrc .bashrc .vim .vimrc .screenrc ~
 git clone https://aszxqw@github.com/aszxqw/oh-my-zsh.git ~/.oh-my-zsh
+cp .zshrc ~/.zshrc
 chsh  -s /bin/zsh

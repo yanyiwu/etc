47957412158f06d6c1db5214a23b2b918a8c5f7d
wyy <wuyanyi09@gmail.com>
2013-10-19 10:16:19 +0800

    add config_zsh

---
+++ linux/config_zsh.sh
@@ -0,0 +1,3 @@
+git clone https://aszxqw@github.com/aszxqw/oh-my-zsh.git ~/.oh-my-zsh
+cp .zshrc .aliases ~
+chsh  -s /bin/zs
--- linux/install.sh
+++ linux/install.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 mkdir ~/.trash
 cp -r .gitconfig .inputrc .bashrc .vim .vimrc .screenrc ~
-git clone https://aszxqw@github.com/aszxqw/oh-my-zsh.git ~/.oh-my-zsh
-cp .zshrc .aliases ~
-chsh  -s /bin/zs

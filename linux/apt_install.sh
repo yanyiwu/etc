f36c1eed102d2cd1718117dbc025b7e67978cc59
wyy <wuyanyi09@gmail.com>
2014-01-25 23:40:42 +0800

    ci

--- linux/apt_install.sh
+++ linux/apt_install.sh
@@ -8,9 +8,10 @@ $INSTALL vim;
 $INSTALL git git-core git-completion;
 $INSTALL ssh screen;
 $INSTALL curl;
+$INSTALL cmake;
 
 #chrome
-$INSTALL chromium-browser;
+#$INSTALL chromium-browser;
 
 #cpp
 $INSTALL g++;
@@ -20,11 +21,11 @@ $INSTALL python-setuptools python-mysqldb pylint
 
 
 #ice communication
-$INSTALL ice34-services ice34-slice ice34-translators;
+#$INSTALL ice34-services ice34-slice ice34-translators;
 
 
 #java
-$INSTALL openjdk-7-jdk;
+#$INSTALL openjdk-7-jdk;
 
 #mysql:need user to set mysql root password
 #$INSTALL mysql-server mysql-server-core mysql-client-core

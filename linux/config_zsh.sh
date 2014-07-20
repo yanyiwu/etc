791480999a794e2b0b9ad66d5cad6fd3dae0eaa0
wyy <wuyanyi09@gmail.com>
2014-07-20 15:54:03 +0800

    ci

--- linux/config_zsh.sh
+++ linux/config_zsh.sh
@@ -1,3 +1,4 @@
 git clone https://aszxqw@github.com/aszxqw/oh-my-zsh.git ~/.oh-my-zsh
 cp .zshrc ~
 chsh  -s /bin/zsh
+#curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh

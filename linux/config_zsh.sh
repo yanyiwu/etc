7cef4e20d4f5421e49179effa6daace4473ed481
wyy <wuyanyi09@gmail.com>
2014-10-20 21:02:45 +0800

    about zsh

--- linux/config_zsh.sh
+++ linux/config_zsh.sh
@@ -1,4 +1,4 @@
-git clone https://aszxqw@github.com/aszxqw/oh-my-zsh.git ~/.oh-my-zsh
+git clone git@github.com:aszxqw/oh-my-zsh.git ~/.oh-my-zsh
 cp .zshrc ~
 chsh  -s /bin/zsh
 #curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh

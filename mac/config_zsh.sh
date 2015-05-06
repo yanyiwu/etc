58bfa87c1ca3930c843901942ba06642240d4c89
yanyiwu <wuyanyi09@foxmail.com>
2015-05-06 22:22:40 +0800

    delete

--- mac/config_zsh.sh
+++
@@ -1,4 +0,0 @@
-git clone git@github.com:yanyiwu/oh-my-zsh.git ~/.oh-my-zsh
-ln -sf ~/etc/mac/.zshrc ~
-chsh  -s /bin/zsh
-#curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh

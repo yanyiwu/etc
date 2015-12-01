c25c5db30dd10a2b9d5f134c2e7ce6a93c141aa5
yanyiwu <wuyanyi09@foxmail.com>
2015-12-01 15:17:12 +0800

    yum install

--- linux/yum_install.sh
+++ linux/yum_install.sh
@@ -1,2 +1,4 @@
-yum install -y vim zsh nc wget cmake
 yum groupinstall -y "Development Tools"
+yum install -y vim zsh nc wget cmake
+yum install -y boost boost-devel boost-doc
+yum install -y automake libtool flex bison pkgconfig gcc-c++ boost-devel libevent-devel zlib-devel python-devel ruby-devel openssl-devel

ba2b05b5c98553a72735fe00695b72a18819741a
yanyiwu <wuyanyi09@foxmail.com>
2015-12-02 15:38:07 +0800

    autojump

--- linux/yum_install.sh
+++ linux/yum_install.sh
@@ -4,3 +4,5 @@ yum install -y boost boost-devel boost-doc
 yum install -y automake libtool flex bison pkgconfig gcc-c++ boost-devel libevent-devel zlib-devel python-devel ruby-devel openssl-devel
 
 rpm -Uvh http://mirrors.kernel.org/fedora-epel/6/i386/epel-release-6-8.noarch.rpm
+
+yum install autojump

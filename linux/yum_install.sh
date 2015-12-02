65bc511ac58ff0b4eac2fabde988e78b7f666d2f
yanyiwu <wuyanyi09@foxmail.com>
2015-12-02 15:37:04 +0800

    epel

--- linux/yum_install.sh
+++ linux/yum_install.sh
@@ -2,3 +2,5 @@ yum groupinstall -y "Development Tools"
 yum install -y vim zsh nc wget cmake
 yum install -y boost boost-devel boost-doc
 yum install -y automake libtool flex bison pkgconfig gcc-c++ boost-devel libevent-devel zlib-devel python-devel ruby-devel openssl-devel
+
+rpm -Uvh http://mirrors.kernel.org/fedora-epel/6/i386/epel-release-6-8.noarch.rpm

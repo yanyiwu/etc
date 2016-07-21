033992d26c1427db92f6602534a1b5cca95b2778
yanyiwu <wuyanyi@bytedance.com>
2016-07-21 15:21:44 +0800

    tmux

--- linux/init_tmux.sh
+++ linux/init_tmux.sh
@@ -1,4 +1,4 @@
-mkdir ~./.tmux
+test -d ~/.tmux || mkdir ~/.tmux
 cd ~/.tmux
 test -d tmux-resurrect || git clone https://github.com/tmux-plugins/tmux-resurrect.git
 test -d tmux-continuum || git clone https://github.com/tmux-plugins/tmux-continuum.git
